7dbe6111e55a8176195332341706464d0fcaaba6
Alexandra Cooper <allie.g.cooper@gmail.com>
2016-10-05 13:45:21 -0400

    Initial Commit

---
+++ ruby/vampires.rb
@@ -0,0 +1,19 @@
+puts "What's your name?"
+
+candidate_name = gets.chomp
+
+puts "How old are you?"
+
+age_in_years = gets.chomp
+
+puts "What year were you born?"
+
+year_of_birth = gets.chomp
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+
+garlic_preference = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance?"
+
+health_needs = gets.chomp
