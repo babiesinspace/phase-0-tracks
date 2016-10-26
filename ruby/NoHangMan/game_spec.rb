f51ede26b1dca0b4418e4b2696dd3ca830133b63
Alexandra Cooper <allie.g.cooper@gmail.com>
2016-10-26 12:21:27 -0400

    Accidentally mess on master

--- ruby/NoHangMan/game_spec.rb
+++ ruby/NoHangMan/game_spec.rb
@@ -1,20 +1,29 @@
 require_relative 'game'
 
 describe NoHangMan do 
+  let(:secret_phrase) {"hello world"}
   let(:game) { NoHangMan.new("hello world") }
 
+  it "changes phrase to blanks" do
+    expect(game.given_phrase).to eq "***** *****"
+  end 
+
   it "determines number of allowed misses" do
-    expect(game.allowed_guesses).to eq 3
+    expect(game.wrong_guesses).to eq 3
   end 
 
   it "determines if your guess is valid" do 
-    game = NoHangMan.new("hello world")
-    expect(game.guess_valid(String)).to eq ([*'a'...'z'])
+    game.guess_valid("a")
+    expect(game.valid_input).to eq true
+    expect(game.guess_valid("a")).to eq "a"
   end 
 
   it "determines if your guess is in the phrase" do
-    game = NoHangMan.new("hello world")
-    expect(user_guess('a')).to eq @given_phrase
+    game.guess_valid("l")
+    game.user_guess("l")
+    expect(game.win_phrase.include?("l")).to eq true
+    expect(game.available_guesses).to eq 3
+    expect(game.given_phrase).to eq "**ll* ***l*"
   end 
 
 end 
