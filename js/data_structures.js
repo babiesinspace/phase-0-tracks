f76ca2d87cb6b1a8e366ea8661f071dec03e7475
Alexandra Cooper <allie.g.cooper@gmail.com>
2016-10-26 17:11:17 -0400

    finish pairing

--- js/data_structures.js
+++ js/data_structures.js
@@ -14,3 +14,28 @@ for (var i = 0; i < colors.length; i++) {
 }
 
 console.log(horsesOfColor);
+
+function Car(make, color, leatherInterior, tiresIncluded) {
+  this.make = make;
+  this.color = color;
+  this.leatherInterior = leatherInterior;
+  this.tiresIncluded = tiresIncluded;
+
+  this.carBeep = function() { console.log( "*beep!*"); };
+
+}
+
+console.log("Make a car FOR ALLIE (NOT Kevin)...");
+var brandNEWCar = new Car("Rabbit-Conv.", "black", true, 5);
+console.log(brandNEWCar);
+console.log("My car can beep!!!: ");
+brandNEWCar.carBeep();
+console.log("-----");
+console.log("Make a car for Kevin...")
+var brandNEWCar = new Car("Gremlin-Geo", "pink", false, 4);
+console.log(brandNEWCar);
+console.log("His car -kinda- beeps! :");
+console.log("feebly") + brandNEWCar.carBeep();
+
+
+
