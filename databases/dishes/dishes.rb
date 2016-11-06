cf5234038873b5e21ac8b7cbba09ccf9703de4cc
Alexandra Cooper <allie.g.cooper@gmail.com>
2016-11-05 22:37:24 -0400

    Patron signup added and tested

--- databases/dishes/dishes.rb
+++ databases/dishes/dishes.rb
@@ -19,7 +19,6 @@ create_dish_table_cmd = <<-SQL_CMD
     name VARCHAR(255),
     location INT,
     description VARCHAR(255),
-    image_file VARCHAR(255),
     seasonal BOOLEAN,
     price VARCHAR(255)
   )
@@ -41,7 +40,7 @@ create_user_table_cmd = <<-SQL_CMD
   CREATE TABLE IF NOT EXISTS user(
     id INTEGER PRIMARY KEY,
     name VARCHAR(255),
-    user_name VARCHAR(255)
+    user_name VARCHAR(255) UNIQUE
   )
 SQL_CMD
 
@@ -57,8 +56,8 @@ def create_restaurant(db, name, location, phone, email, comments)
   db.execute("INSERT INTO restaurants (name, location, phone, email, comments) VALUES (?, ?, ?, ?, ?)", [name, location, phone, email, comments])
 end
 
-def create_dish(db, name, location, description, image, seasonal, price)
-  db.execute("INSERT INTO dish (name, location, description, image_file, seasonal, price) VALUES (?, ?, ?, ?, ?, ?)", [name, location, description, image, seasonal, price])
+def create_dish(db, name, location, description, seasonal, price)
+  db.execute("INSERT INTO dish (name, location, description, seasonal, price) VALUES (?, ?, ?, ?, ?)", [name, location, description, image, seasonal, price])
 end
 
 def create_review(db, stars, accuracy, consistency, comments, user_id, dish_id)
@@ -68,3 +67,22 @@ end
 def create_user(db, name, user_name)
   db.execute("INSERT INTO user (name, user_name) VALUES (?, ?)", [name, user_name])
 end
+
+def patron_signup(db)
+  info_correct = false
+  until info_correct
+    puts "Ever get annoyed when that hole in the wall with amazing food only has 2 stars on Yelp?..\nEver wonder what the locals are eating?..\nAre you a constant over-orderer?\nSick of your friends winning the dinner order?\nCould you look at a menu all day wondering?\nSign up today! Dish about it.\nWhat's your name?"
+    patron_name = gets.chomp!
+    puts "What should your handle be?"
+    user_handle = gets.chomp!
+    puts "#{patron_name}\nHandle:#{user_handle}\nIs this correct?"
+    correct = gets.chomp!
+    if correct == "yes"
+      create_user(db, patron_name, user_handle)
+      info_correct = true 
+    end
+    valid_input = true 
+  end 
+end
+
+patron_signup(db)
